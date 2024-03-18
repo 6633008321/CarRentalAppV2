35c64fcdf4d1933c454b4296dfb1faf37b2af294
Boon <6633008321@student.chula.ac.th>
2024-03-18 22:13:31 +0700

    w10

--- src/app/reservations/manage/@dashboard/page.tsx
+++ src/app/reservations/manage/@dashboard/page.tsx
@@ -1,11 +1,39 @@
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import getUserProfile from '@/libs/getUserProfile';
+import { getServerSession } from 'next-auth';
 import React from 'react'
 
-function DashboardPage() {
-  return (
-    <main className='bg-slate-100'>
-        <div>Your Dashboard</div>
-    </main>
-  )
-}
+export default async function DashboardPage() {
+    const session = await getServerSession(authOptions);
 
-export default DashboardPage
+    if(!session || !session.user.token)  return null;
+
+    const profile = await getUserProfile(session.user.token);
+    var createdAt = new Date(profile.data.createdAt);
+
+    return (
+        <main className="bg-slate-100 m-5 p-5">
+            <div className="text-xl">{profile.data.name}</div>
+            <table className="table-auto border-separate border-spacing-2">
+                <tbody>
+                    <tr>
+                        <td>Email</td>
+                        <td>{profile.data.email}</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>Tel.</td>
+                        <td>{profile.data.tel}</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>Member Since</td>
+                        <td>{createdAt.toString()}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </main>
+    );
+}
